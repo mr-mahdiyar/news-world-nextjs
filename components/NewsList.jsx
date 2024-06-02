a89a544768aaa8a9e9746032b943119430366aa3
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-02 10:50:47 +0330

    Link addresses  were editted.

--- components/NewsList.jsx
+++ components/NewsList.jsx
@@ -3,7 +3,7 @@ export default function NewsList ({news}) {
     return <ul className="news-list">
     {news.map((newsItem) => (
       <li key={newsItem.id}>
-        <Link href={`news/${newsItem.slug}`}>
+        <Link href={`/news/${newsItem.slug}`}>
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
           <span>{newsItem.title}</span>
         </Link>
