da1157810c9e04fdc961985800486cb2e41e1fd9
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-02 10:18:03 +0330

    NewsList section was added to seperate file and added as a seperate file to NewsPage.

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,3 +1,4 @@
+import NewsList from "@/components/NewsList";
 import { DUMMY_NEWS } from "@/dummy-news";
 import Link from "next/link";
 
@@ -5,16 +6,7 @@ export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`news/${newsItem.slug}`}>
-              <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }
