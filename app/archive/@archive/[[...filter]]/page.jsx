2405a50a4b1768f43b07f8de2bc963fe29b87417
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-02 12:27:22 +0330

    Folder name was changed.

---
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -0,0 +1,53 @@
+import NewsList from "@/components/NewsList";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+import Link from "next/link";
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+  const selectedYear = filter?.[0];
+  const selectedMounth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMounth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear)
+  }
+
+  
+  if (selectedYear && selectedMounth) {
+      news = getNewsForYearAndMonth(selectedYear, selectedMounth);
+      links = []
+    }
+    let newsContent = <p>No news found for the selected period.</p>;
+     
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
+
+  
+    return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+                const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
