c96f52cf64d128e0aebdc74fcf9fe6b2d0638c69
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-02 08:59:03 +0330

    News list section was added.

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,3 +1,20 @@
-export default function NewsPage () {
-    return <h1>News Page</h1>
-  }
+import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`news/${newsItem.slug}`}>
+              <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
