4b5cd8dd6b21c903588896c8c74bfaf8b7b81600
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-02 10:23:07 +0330

    Link was removed from NewsPage and added to NewsList.

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,6 +1,5 @@
 import NewsList from "@/components/NewsList";
 import { DUMMY_NEWS } from "@/dummy-news";
-import Link from "next/link";
 
 export default function NewsPage() {
   return (
--- components/NewsList.jsx
+++ components/NewsList.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 export default function NewsList ({news}) {
     return <ul className="news-list">
     {news.map((newsItem) => (
