ad1b7a72cb76904cd2e49937ca84335fead877a8
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-05 18:15:26 +0330

    NavLink(s) were added to header.

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import NavLink from './NavLink';
 
 export default function MainHeader() {
   return (
@@ -9,7 +10,10 @@ export default function MainHeader() {
       <nav>
         <ul>
           <li>
-            <Link href="/news">News</Link>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink href="/archive">Archive</NavLink>
           </li>
         </ul>
       </nav>
