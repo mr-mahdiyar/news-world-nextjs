a8a5b340941d7fa775e869e36d7e5c1d37c34dc2
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-06 11:08:52 +0330

    Parallel routes were added.

---
+++ app/news/[slug]/@modal/(.)image/page.jsx
@@ -0,0 +1,23 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+export default function ImagePage({ params }) {
+  const newsItemSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find(
+    (newsItem) => newsItem.slug === newsItemSlug
+  );
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}/`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+}
