287cd47bf3d7b78d5b3020f4a618448d994d78c0
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-06 08:45:52 +0330

    ImagePage that must show images in full screen size was added.

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,15 +1,19 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 export default function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
   if(!newsItem) {
     notFound();
   }
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+        <img src={`/images/news/${newsItem.image}/`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}></time>
       </header>
